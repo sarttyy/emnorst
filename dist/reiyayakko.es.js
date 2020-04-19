97fa8f9fb98b9c152a951bd0bf658b24d6bace07
reiyayakko <reiyayakko@gmail.com>
2020-04-20 01:34:59 +0900

    math bug fix if not number error isPrime if 2 return true diff not use buildin Math

--- dist/reiyayakko.es.js
+++ dist/reiyayakko.es.js
@@ -223,7 +223,7 @@ const max = (...args)=>(
         max < value ? max : value
     ), -Infinity)
 );
-const diff = (n1, n2)=>Math.abs(n1-n2);
+const diff = (n1, n2)=>abs(n1-n2);
 const factorial = num=>{
     for(let i = num;i > 2;num *= --i);
     return num ? num : ++num;
@@ -235,21 +235,25 @@ const fibonacci$ = frequency=>{
     return Math.round((x - y) / sqrt5);
 };
 const fibonacci = function* (frequency=Infinity){
-    // @ts-ignore
     for(let prev = 1n, fib = 0n;frequency--;)
         yield fib = prev + (prev = fib);
 };
 const isPrime = number=>{
+    if(number === 2)
+        return true;
     if(Number.isNaN(number) || !Number.isFinite(number) || number < 2 || number % 2 === 0)
         return false;
     for(let i = 3, sqrt = Math.sqrt(number);i <= sqrt;i += 2)
         if(number % i === 0)return false;
     return true;
 };
+// TODO: 素数ジェネレーター
 const prime = number=>new Promise((resolve, reject)=>{
     // INFO: 素因数分解
-    if(Number.isNaN(number) || !Number.isFinite(number))
-        reject();
+    if(Number.isNaN(number) || !Number.isFinite(number) || typeof number !== "number"){
+        reject(new Error("Only the numerical value can be factorized"));
+        return;
+    }
     const result = [];
     while(number % 2 === 0){
         result.push(2);
@@ -260,6 +264,7 @@ const prime = number=>new Promise((resolve, reject)=>{
             result.push(i);
             number /= i;
         }
+    if(number > 1)result.push(number);
     resolve(result);
 });
 /*
