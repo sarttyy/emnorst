cf55578bd6880cb85fbcb7dcea693e930433ad30
reiyayakko <reiyayakko@gmail.com>
2020-06-10 10:29:33 +0900

    Enigma preparation for multi-byte char

--- src/util/string/enigma.js
+++ src/util/string/enigma.js
@@ -1,56 +1,89 @@
 
 // @ts-check
 
-const difaultGears = [
+import { isOdd } from "../is/number";
+
+const Gears = [
     [16,14,6,3,4,20,9,24,7,2,23,1,25,8,19,0,5,12,15,18,21,11,13,10,17,22],
     [9,7,3,24,17,10,1,13,15,22,2,25,5,14,19,4,0,20,18,23,11,16,8,12,21,6],
     [11,12,25,3,0,10,21,19,8,24,2,15,9,22,18,17,23,14,1,4,13,16,7,6,20,5],
 ];
-const difaultR = [1,0,3,2,5,4,7,6,9,8,11,10,13,12,15,14,17,16,19,18,21,20,23,22,25,24];
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const reverse = (n) => isOdd(n) ? n + 1 : n - 1;
+const table = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+/**
+ * @example
+ * const hoge = new rei.Enigma();
+ * hoge.exchange("hello world");
+ * // => "fbuuqrpedw"
+ * hoge.exchange("hello world");
+ * // => "llxrrdwecp"
+ *
+ * hoge.rotate(true); // reset rotation
+ * hoge.exchange("fbuuqrpedw");
+ * // => "helloworld"
+ * hoge.exchange("llxrrdwecp");
+ * // => "helloworld"
+ */
 export class Enigma {
-    constructor(charTable=alphabet, gears=difaultGears, reverseGear=difaultR) {
+    /**
+     * @param {string} charTable
+     * @param {number[][]} gears
+     * @param {number[] | function(number): number} reverseGear
+     */
+    constructor(charTable=table, gears=Gears, reverseGear=reverse) {
         this.charTable = charTable;
         this.gears = gears;
-        this.reverseGear = reverseGear;
+        this.reverse = typeof reverseGear === "function"
+            ? reverseGear : (i) => reverseGear[i];
         this.rotation = new Array(this.gears.length).fill(0);
     }
-    _place(i, n) {
-        const { length } = this.gears[i];
-        return (length + n - this.rotation[i]) % length;
-    }
-    _shift(i, n) {
-        return (this.rotation[i] + n) % this.gears[i].length;
-    }
-    charExchange(char) {
-        const l = this.gears.length;
-        let i = 0, n = this.charTable.indexOf(char);
-        if(!~n)return null;
-        for(;i < l;i++) {
-            const location = this._place(i, n);
-            n = this._shift(i, this.gears[i][location]);
-        }
-        // 反転
-        n = this.reverseGear[n];
-        for(;i--;) {
-            const location = this._place(i, n);
-            n = this._shift(i, this.gears[i].indexOf(location));
-        }
-        // ギアの回転
-        for(let i = 0, flag = true;flag && i < l;) {
+    /**
+     * @param {boolean} [reset]
+     */
+    rotate(reset) {
+        if(reset) this.rotation.fill(0);
+        else for(let i = 0, flag = true;flag && i < this.gears.length;) {
             flag = ++this.rotation[i] === this.gears[i].length;
             flag && (this.rotation[i++] = 0);
         }
-        return this.charTable[n] || null;
+    }
+    /**
+     * @param {number} n
+     */
+    convert(n) {
+        let i = 0;
+        if(!~n) return null;
+        for(;i < this.gears.length;i++) {
+            const { length } = this.gears[i];
+            const location = (length + n - this.rotation[i]) % length;
+            n = (this.rotation[i] + this.gears[i][location]) % length;
+        }
+        // 反転
+        n = this.reverse(n);
+        for(;i--;) {
+            const { length } = this.gears[i];
+            const location = (length + n - this.rotation[i]) % length;
+            n = (this.rotation[i] + this.gears[i].indexOf(location)) % length;
+        }
+        // ギアの回転
+        this.rotate();
+        return n;
     }
     /**
      * @param {string} string
      */
     exchange(string) {
         let cryptedStr = "";
-        for(let i = 0;i < string.length;i++)
-            cryptedStr += this.charExchange(string[i]);
+        for(let i = 0;i < string.length;i++) {
+            let n = this.charTable.indexOf(string[i]);
+            if(!~n) continue;
+            if(!Array.isArray(n))
+                n = this.convert(n);
+            else for(let j = 0;j < n.length;j++)
+                n[j] = this.convert(n[j]);
+            cryptedStr += this.charTable[n];
+        }
         return cryptedStr;
     }
 }
