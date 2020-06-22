dc2618ea9291f585aae11984ad46b4f927cd88c8
reiyayakko <reiyayakko@gmail.com>
2020-06-22 23:24:01 +0900

    split beta

--- src/utility/loop/each/chunk.js
+++ src/utility/loop/each/chunk.js
@@ -1,7 +1,7 @@
 
 // @ts-check
 
-import { isFunction } from "../../../util/is/index";
+import { isFunction } from "../../../util/is/type";
 import { assertType } from "../../condition/index";
 import { previous } from "../order";
 
@@ -17,7 +17,7 @@ const not = (value)=>(
  */
 export const chunk = (array, size)=>{
     assertType(array, ["String", "Array"]);
-    assertType(size, "Number");
+    assertType(size, Number.isSafeInteger);
     let index = 0;
     return previous(Math.ceil(array.length / size), (result)=>{
         result.push(array.slice(index, (index += size)));
@@ -30,9 +30,12 @@ export const chunk = (array, size)=>{
  */
 export const split = (array, count)=>{
     assertType(array, ["String", "Array"]);
-    assertType(count, "Number");
-    let index = 0;
+    assertType(count, Number.isSafeInteger);
+    let extra = array.length % count, index = 0;
+    const size = (array.length - extra) / count;
     return previous(count, (result)=>{
-        result.push(array.slice(index, (index += Math.round(array.length / count))));
+        const pad = Number(extra-- > 0);
+        const _ = array.slice(index, (index += size + pad));
+        result.push(_);
     }, []);
 };
