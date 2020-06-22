9a6eeebe50baeffae05b37460cd41652e4a23ec9
reiyayakko <reiyayakko@gmail.com>
2020-06-22 23:36:55 +0900

    fix types

--- src/util/typeof.js
+++ src/util/typeof.js
@@ -1,55 +1,15 @@
 
-/**
- * @typedef {"Int8Array"|
- * "Uint8Array"|
- * "Uint8ClampedArray"|
- * "Int16Array"|
- * "Uint16Array"|
- * "Int32Array"|
- * "Uint32Array"|
- * "Float32Array"|
- * "Float64Array"} TypedArray
- */
+// @ts-check
 
-/**
- * @typedef {"Map" | "Set" | "WeakMap" | "WeakSet"} MapSet
- */
-
-/**
- * デフォルトで`Object.prototype.toString.call`の返り値として考えられるすべてのパターンです。
- * たぶん。
- *
- * @typedef {"Null"|
- * "Undefined"|
- * "Number"|
- * "String"|
- * "Boolean"|
- * "Symbol"|
- * "Function"|
- * "Object"|
- * "Array"|
- * "Arguments"|
- * "RegExp"|
- * "Date"|
- * "Math"|
- * "JSON"|
- * "Error"|
- * "Promise"|
- * "GeneratorFunction" | "Generator"|
- * MapSet |
- * TypedArray | "ArrayBuffer" |
- * "DataView"}
- * ObjectToStringTypes
- */
+const { toString } = Object.prototype;
 
 /**
  * You can add the type by changing the "Symbol.toStringTag" property of the object.
- * オブジェクトの"Symbol.toStringTag"プロパティーを変更することで、型を追加できます。
  *
- * thinking: 返り値はSymbol.toStringTagを考慮してstring型にすべきか？
+ * オブジェクトの"Symbol.toStringTag"プロパティーを変更することで、型を追加できます。
  * @param {*} value An object that determines the type
- * @return {ObjectToStringTypes & string} object type
+ * @return {string} object type
  */
-export const typeOf = (value)=>(
-    Object.prototype.toString.call(value).slice(8, -1)
+export const typeOf = (value) => (
+    toString.call(value).slice(8, -1)
 );
