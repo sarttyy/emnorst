68a01dda9a4df2f080f175361dd90c9113e17b00
reiyayakko <reiyayakko@gmail.com>
2020-06-27 16:22:56 +0900

    +simple fns

--- src/util/function/index.js
+++ src/util/function/index.js
@@ -1,3 +1,4 @@
 
+export * from "./simple/index.js";
 export * from "./benchmark.js";
 export * from "./thinning.js";
---
+++ src/util/function/simple/arguments.js
@@ -0,0 +1,4 @@
+
+export const Arguments = function() {
+    return arguments;
+};
