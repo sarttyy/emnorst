e9b5ff3d2f807fda4c580e381657c459da3f1dab
reiyayakko <reiyayakko@gmail.com>
2020-07-15 16:50:16 +0900

    +const

---
+++ src/util/const/string.js
@@ -0,0 +1,5 @@
+
+export const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+export const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const decimal = "0123456789";
+export const hex = decimal + "abcdef";
