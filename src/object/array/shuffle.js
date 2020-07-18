1be0d94a7eb9fae8871f97cd2fd17f5c2d7860dd
reiyayakko <reiyayakko@gmail.com>
2020-07-18 19:40:18 +0900

    Reflect ideas

--- src/object/array/shuffle.js
+++ src/object/array/shuffle.js
@@ -5,8 +5,7 @@ import { swap } from "../property/swap";
 import { xorshift } from "../../util/random/xorshift";
 
 export const shuffle = (data) => {
-    for(let i = data.length;i--;) {
-        // IDEA: data.length - 1
+    for(let i = data.length - 1;i--;) {
         const r = xorshift(0, i);
         swap(data, i, r);
     }
