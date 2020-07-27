e5af200df62bcbb13afd7788658fa65538054b40
reiyayakko <reiyayakko@gmail.com>
2020-07-28 01:30:07 +0900

    +edit(assign object)

--- src/object/property/swap.js
+++ src/object/property/swap.js
@@ -1,14 +1,19 @@
 
-// @ts-check
+/*
+オブジェクトの指定したプロパティを入れ替えます。
+
+このオブジェクトに対して代入します。指定しない場合、 `origin`が指定されます。
+*/
 
 /**
- * @param {*} orign
+ * @param {*} orign Swaps the specified properties of the object.
  * @param {PropertyKey} m
  * @param {PropertyKey} n
+ * @param {*} [edit] Assign to this object. If not specified, `origin` is specified.
  */
-export const swap = (orign, m, n)=>{
+export const swap = (orign, m, n, edit=orign) => {
     const temp = orign[m];
-    orign[m] = orign[n];
-    orign[n] = temp;
-    return orign;
+    edit[m] = orign[n];
+    edit[n] = temp;
+    return edit;
 };
