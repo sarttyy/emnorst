cf3450b55f8bc372bf25518f17b9f36ca1326e03
reiyayakko <reiyayakko@gmail.com>
2020-07-28 19:49:12 +0900

    Fixed forgetting to write export

--- src/util/is/string/lower-letter.js
+++ src/util/is/string/lower-letter.js
@@ -2,7 +2,7 @@
 import { isInteger } from "../number/integer.js";
 import { isString } from "./string.js";
 
-const isLowerLetter = (charCode) => (
+export const isLowerLetter = (charCode) => (
     isString(charCode)
         ? isLowerLetter(charCode.charCodeAt())
         : charCode > 0x60 && charCode <= 0x7a
--- src/util/is/string/upper-letter.js
+++ src/util/is/string/upper-letter.js
@@ -2,7 +2,7 @@
 import { isInteger } from "../number/integer.js";
 import { isString } from "./string.js";
 
-const isUpperLetter = (charCode) => (
+export const isUpperLetter = (charCode) => (
     isString(charCode)
         ? isUpperLetter(charCode.charCodeAt())
         : charCode > 0x40 && charCode <= 0x5a
