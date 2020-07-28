ebcb3d199fdb72273b95ad2da40fe79ef9958e4a
reiyayakko <reiyayakko@gmail.com>
2020-07-28 23:43:19 +0900

    use copyType for copy root object

--- src/object/zip.js
+++ src/object/zip.js
@@ -3,36 +3,33 @@
 
 import { isArrayLike } from "../util/is/index.js";
 import { forOf } from "../utility/loop/for-of.js";
+import { copyType } from "./clone/copyType.js";
+
+/*
+オブジェクトを深さ0と1で反転します。
+
+ターゲットオブジェクト。 このオブジェクトの子プロパティもオブジェクトである必要があります。
+
+オブジェクトのキーを配列として返す関数。
+
+深度が0と1で反転したオブジェクト。
+*/
 
 /**
  * Flip objects at depth 0 and 1.
- *
- * オブジェクトを深さ0と1で反転します。
- *
  * @param {*} objects
  * The target object. The child properties of this object must also be objects.
- *
- * ターゲットオブジェクト。 このオブジェクトの子プロパティもオブジェクトである必要があります。
  * @param {function(any): PropertyKey[]} getKeys
  * A function that returns the keys of the object as an array.
- *
- * オブジェクトのキーを配列として返す関数。
- * {@link allKeys}などが指定できます
  * @return {object}
  * An object whose depth is inverted by 0 and 1.
- *
- * 深度が0と1で反転したオブジェクト。
  */
-export const zip = (objects, getKeys=Object.keys)=>{
+export const zip = (objects, getKeys=Object.keys, fromRight=false) => {
     const keys = getKeys(objects);
-    const rootIsArray = isArrayLike(objects);
-    const isArray = keys.every((key)=>(
-        isArrayLike(objects[key])
-        // && getKeys(objects[key]).every((_)=>/\d/.test(_))
-    ));
-    return keys.reduce((ziped, key)=>{
-        forOf(getKeys(objects[key]), (name)=>{
-            if(!ziped[name])ziped[name] = rootIsArray ? [] : {};
+    const isArray = keys.every((key) => isArrayLike(objects[key]));
+    return keys.reduce((ziped, key) => {
+        forOf(getKeys(objects[key]), (name) => {
+            if(!ziped[name]) ziped[name] = copyType(objects);
             ziped[name][key] = objects[key][name];
         });
         return ziped;
