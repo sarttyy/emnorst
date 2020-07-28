9cc88f9abfe0abcd0e611e921fdc0b9e2bc0e49c
reiyayakko <reiyayakko@gmail.com>
2020-07-28 19:45:42 +0900

    fix operator priority

--- src/util/is/number/negative.js
+++ src/util/is/number/negative.js
@@ -3,6 +3,5 @@ import { is } from "../equals/is.js";
 import { isNumber } from "./number.js";
 
 export const isNegative = (number) => (
-    isNumber(number) && number < 0
-        || is(-0, Number(number))
+    isNumber(number) && (number < 0 || is(-0, Number(number)))
 );
