d3610a8fb991f3aa1081c0d779c2d74ba7a1ca68
reiyayakko <reiyayakko@gmail.com>
2020-07-31 15:46:21 +0900

    remake iterate to util/loop add iterator add iterable

---
+++ src/util/loop/iterate/iterable.js
@@ -0,0 +1,18 @@
+
+import * as context from "../../../env/symbol.js";
+import { iterator } from "./iterator.js";
+
+/**
+ * Iterable
+ */
+class iterable {
+    constructor(iterateData, next) {
+        this.iterateData = iterateData;
+        if(next) this.iterateNext = next;
+    }
+    [context.Symbol.iterator]() {
+        return iterator(this.iterateData, this.iterateNext);
+    }
+}
+
+export { iterable as Iterable };
---
+++ src/util/loop/iterate/iterate.js
@@ -0,0 +1,27 @@
+
+import * as context from "../../../env/symbol.js";
+import { isArrayLike } from "../../is/object/array-like.js";
+import { isFunction } from "../../is/other/function.js";
+import { iterator } from "./iterator.js";
+
+const next = (data, i) => ({
+    done: i < data.length,
+    value: data[i]
+});
+
+/**
+ * iterableなオブジェクトをiteratorに変換します。
+ * @template T
+ * @param {Iterable<T> | ArrayLike<T>} iterable
+ * @return {Iterator<T, null>}
+ * @throws {TypeError}
+ */
+export const iterate = (iterable) => {
+    if(isFunction(iterable[context.Symbol.iterator]))
+        return iterable[context.Symbol.iterator]();
+    if(isFunction(iterable["@@iterator"]))
+        return iterable["@@iterator"]();
+    if(isArrayLike(iterable))
+        return iterator(iterable, next);
+    throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+};
