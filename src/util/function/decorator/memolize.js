68bfb101bd8ffad40bde426d69fc70c810b60e86
reiyayakko <reiyayakko@gmail.com>
2020-08-07 00:14:23 +0900

    add a process when there is no argments allow modify of equals function

--- src/util/function/decorator/memolize.js
+++ src/util/function/decorator/memolize.js
@@ -2,18 +2,20 @@
 import { Each } from "../../loop/base/each-class.js";
 import { equals } from "../../is/equals/equals.js";
 
-export const memolize = (func) => {
+export const memolize = (func, eq=equals) => {
     const cache = {};
     return function() {
         const { length } = arguments;
-        if(cache[length]) {
-            const fn = (arg, i) => equals(arg, arguments[i]);
-            for(const each = new Each(cache[length], { mode: "iterable" });each.continue();)
+        if(length in cache) {
+            if(length === 0) return cache[0];
+            const fn = (arg, i) => eq(arg, arguments[i]);
+            for(const each = new Each(cache[length], "iterable");each.continue();)
                 if(each.current.value[0].every(fn))
                     return each.current.value[1];
         }else cache[length] = new Map();
-        const result = func(...arguments);
-        cache[length].set([...arguments], result);
+        const result = func.apply(this, arguments);
+        if(length === 0) cache[0] = result;
+        else cache[length].set([...arguments], result);
         return result;
     };
 };
