942ceb1a9a33556a0d17991d984f3e8c88aa8c63
reiyayakko <reiyayakko@gmail.com>
2020-08-09 20:22:37 +0900

    optimize isPositive, isNegative

--- src/util/is/number/positive.js
+++ src/util/is/number/positive.js
@@ -3,5 +3,7 @@ import { is } from "../equals/is.js";
 import { isNumber } from "./number.js";
 
 export const isPositive = (number) => (
-    isNumber(number) && (number > 0 || is(0, Number(number)))
+    isNumber(number) && (
+        number > 0 || number === 0 && is(0, +number)
+    )
 );
