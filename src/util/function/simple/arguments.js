4132ec3decb7c749af224302503721d23fff0c5c
reiyayakko <reiyayakko@gmail.com>
2020-08-12 23:11:33 +0900

    add doc to function/simple/arguments

--- src/util/function/simple/arguments.js
+++ src/util/function/simple/arguments.js
@@ -1,4 +1,10 @@
 
+// 引数はJavaScriptエンジンによる最適化を妨げる可能性があります。
+
+/**
+ * arguments may hinder with optimization by the JavaScript engine.
+ * @return {IArguments} the `arguments` directly
+ */
 export const Arguments = function() {
     return arguments;
 };
