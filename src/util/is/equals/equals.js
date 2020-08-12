455a2c09a1b72e8c300d023a832bd15b330f8bca
reiyayakko <reiyayakko@gmail.com>
2020-08-12 22:45:45 +0900

    optimize equals

--- src/util/is/equals/equals.js
+++ src/util/is/equals/equals.js
@@ -1,15 +1,19 @@
 
+/* eslint-disable no-self-compare */
+
 // SameValueZeroアルゴリズムを使用して、値がすべて等しいことを検証します。
 
 /**
  * Validate that the values are all equal using the SameValueZero algorithm.
+ * @param {any} first
  * @param {...any} values
  * @return {boolean}
  */
-export const equals = (...values) => {
-    const first = values.shift();
-    const equal = Number.isNaN(first)
-        ? Number.isNaN
-        : (next) => (first === next);
-    return values.every(equal);
+export const equals = (first, ...values) => {
+    let i = 0;
+    const size = values.length;
+    if(first === first)
+        while(first === values[i] && i < size) ++i;
+    else while(values[i] !== values[i]) ++i;
+    return i === size;
 };
