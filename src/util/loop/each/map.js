471341b1d0e94b3139f2c7d6b70479b16d330180
reiyayakko <reiyayakko@gmail.com>
2020-08-13 01:05:29 +0900

    add support whether to copy

--- src/util/loop/each/map.js
+++ src/util/loop/each/map.js
@@ -2,8 +2,8 @@
 import { Each } from "../base/each-class.js";
 import { copyType } from "../../../object/clone/copyType.js";
 
-export const map = function(items, fn) {
-    const maped = copyType(items);
+export const map = function(items, fn, copy=true) {
+    const maped = copy ? copyType(items) : items;
     for(const each = new Each(items);each.continue();) {
         const { current } = each;
         const key = current.key || current.index;
