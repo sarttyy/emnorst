4e708075487173d69574cfea1e6e07411034335e
reiyayakko <reiyayakko@gmail.com>
2020-08-20 21:40:31 +0900

    Includes modeAnalysis

--- src/util/loop/base/each-class.js
+++ src/util/loop/base/each-class.js
@@ -1,15 +1,20 @@
 
-// @ts-check
-
-import { getKeys } from "../../../object/property/keys.js";
-import { isFunction } from "../../is/other/function.js";
+import { getKeys } from "../../../object/standard/keys.js";
+import { isArrayLike } from "../../is/object/array-like.js";
+import { isArray } from "../../is/object/array.js";
+import { isObject } from "../../is/object/object.js";
+import { isIterable } from "../../is/other/iterable.js";
 import { iterate } from "../iterate/iterate.js";
 import { nexts } from "./each-nexts.js";
-import { modeAnalysis } from "./mode-analysis.js";
 
 /** @typedef {"object" | "arraylike" | "iterable" | "auto"} mode */
 
+const ARRAYLIKE = "arraylike";
+const ITERABLE = "iterable";
+const OBJECT = "object";
+
 /**
+ * @template T
  * @example
  * for(const each = new Each();each.continue();) {
  *     const { value, count } = each.current;
@@ -27,29 +32,62 @@ export class Each {
     constructor(eachItems, props={}) {
         if(eachItems == null)
             throw new Error("eachItems is not eachable");
-        const mode = this.mode = modeAnalysis(eachItems, props.mode || props);
-        switch(mode) {
-        case "object":
-            this.keys = isFunction(props.keys)
-                ? (props.keys || getKeys)(eachItems)
-                : props.keys;
-            break;
-        case "arraylike":
-            this.reverse = !!props.reverse;
-            break;
-        case "iterable":
-            this.iterator = iterate(eachItems);
-            break;
-        }
         this.eachItems = eachItems;
         this.done = false;
+        const reverse = !!props.reverse;
+        /**
+         * @type {object}
+         * @property {T} value
+         */
         this.current = {
-            count: this.reverse ? eachItems.length : -1
+            count: reverse ? eachItems.length : -1
         };
+        const keys = props.keys || props;
+        let mode = props.mode || props;
+        if(!(mode in nexts)) {
+            mode = isArrayLike(eachItems) ? ARRAYLIKE
+                : isIterable(eachItems) ? ITERABLE
+                : OBJECT;
+        }
+        this.mode = mode;
+        switch(mode) {
+        case ARRAYLIKE:
+            this.reverse = reverse;
+            break;
+        case ITERABLE:
+            this.iterator = iterate(eachItems);
+            break;
+        case OBJECT:
+            this.keys = isArray(keys) ? keys
+                : (typeof keys === "function" ? keys : getKeys)(eachItems);
+            break;
+        }
     }
     continue() {
         if(this.done) return false;
         nexts[this.mode](this);
         return !this.done;
+    }
+    static modeAnalysis(eachItems, mode) {
+        if(mode in nexts) return mode;
+        if(!isArray(mode))
+            mode = [ARRAYLIKE, ITERABLE, OBJECT];
+
+        for(let i = 0;i < mode.length;)
+            switch(mode[i++]) {
+            case ARRAYLIKE:
+                if(isArrayLike(eachItems))
+                    return ARRAYLIKE;
+                break;
+            case ITERABLE:
+                if(isIterable(eachItems))
+                    return ITERABLE;
+                break;
+            case OBJECT:
+                if(isObject(eachItems))
+                    return OBJECT;
+                break;
+            }
+        return null;
     }
 }
