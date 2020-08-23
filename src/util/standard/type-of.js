55c8cf5ec8f69410a2152d6c453c06c2dfb5be77
reiyayakko <reiyayakko@gmail.com>
2020-08-23 18:58:43 +0900

    add cache

--- src/util/standard/type-of.js
+++ src/util/standard/type-of.js
@@ -3,14 +3,20 @@
 
 const { toString } = Object.prototype;
 
+let prev = NaN, result;
+
 // `Object.prototype.toString`を使用して値の型を取得します。
 
 /**
  * Use `Object.prototype.toString` to get the value type.
  *
- * @param {any} value
- * @return {string}
+ * @param {any} value Value to get the type
+ * @return {string} String of type
  */
-export const typeOf = (value) => (
-    toString.call(value).slice(8, -1)
-);
+export const typeOf = (value) => {
+    if(prev === value)
+        return result;
+    prev = value;
+    result = toString.call(value).slice(8, -1);
+    return result;
+};
