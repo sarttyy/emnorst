665d27422fa3e13b2bb0f180509aee3de54c865e
reiyayakko <reiyayakko@gmail.com>
2020-11-22 07:04:03 +0900

    fix code style

--- src/object/standard/prototype.ts
+++ src/object/standard/prototype.ts
@@ -1,5 +1,6 @@
 
 export const { prototype: ObjectPrototype } = Object;
 export const { toString } = ObjectPrototype;
+
 export const { prototype: ArrayPrototype } = Array;
 export const { slice, push, pop } = ArrayPrototype;
