8469f72958b1e21fbd1d0f108f15c6fbd2fe3ea6
reiyayakko <reiyayakko@gmail.com>
2020-12-01 11:08:56 +0900

    add toPropertyKey

--- src/object/property/index.ts
+++ src/object/property/index.ts
@@ -3,4 +3,5 @@ export * from "./has";
 // export * from "./pick";
 export * from "./property";
 export * from "./swap";
+export * from "./to-property-key";
 
---
+++ src/object/property/to-property-key.ts
@@ -0,0 +1,7 @@
+
+import { toPrimitive } from "../../util/standard/to-primitive";
+
+export const toPropertyKey = (value: unknown): PropertyKey => {
+    const key = toPrimitive(value, "string");
+    return typeof key === "symbol" ? key : String(key);
+};
