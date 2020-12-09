18e1b52c6e8529c99e9157c1ec78b92c03222ec7
reiyayakko <reiyayakko@gmail.com>
2020-12-09 15:57:13 +0900

    change overload to generics extends

--- src/object/arraylike/util/shuffle.ts
+++ src/object/arraylike/util/shuffle.ts
@@ -3,11 +3,8 @@ import { xorshift } from "../../../number/random/xorshift";
 import { copyBase } from "../../clone";
 import { swap } from "../../property/swap";
 
-export const shuffle: {
-    <T>(origin: T[], copy?: boolean): T[];
-    <T>(origin: ArrayLike<T>, copy?: boolean): ArrayLike<T>;
-} = <T>(origin: ArrayLike<T>, copy=false) => {
-    const result = copy ? copyBase(origin) : origin;
+export const shuffle = <T extends ArrayLike<T>>(origin: T, copy=false): T => {
+    const result = copy ? copyBase(origin) as T : origin;
     for(let i = origin.length;i--;) {
         const rand = xorshift(0, i);
         swap(origin, i, rand, result);
