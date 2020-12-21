c07db0da4f5d0709c2b1de8efc78c27b6bedf92c
reiyayakko <reiyayakko@gmail.com>
2020-12-22 07:22:54 +0900

    fix style

--- src/object/standard/keys.ts
+++ src/object/standard/keys.ts
@@ -2,7 +2,6 @@
 export const { keys } = Object;
 
 const getPropNames = Object.getOwnPropertyNames;
-
 const getSymbols = Object.getOwnPropertySymbols;
 
 const { propertyIsEnumerable } = Object.prototype;
@@ -15,18 +14,22 @@ const { propertyIsEnumerable } = Object.prototype;
  */
 export const getKeys = (obj: object): PropertyKey[] => {
     const keys$: PropertyKey[] = keys(obj);
+
     const symbols = getSymbols(obj);
     for(let i = 0;i < symbols.length;i++) {
         const symbol = symbols[i];
-        if(propertyIsEnumerable.call(obj, symbol))
+
+        if(propertyIsEnumerable.call(obj, symbol)) {
             keys$.push(symbol);
+        }
     }
+
     return keys$;
 };
 
 /**
- * IDEA: rename ownKeys
  * Returns all properties of the object.
+ * IDEA: rename ownKeys
  *
  * @param obj Object to get properties.
  * @return An array of all properties, including symbols and non-enumerable.
