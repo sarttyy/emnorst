5e5a85b2e417e81cb99503f7fa15c3e33fee5220
reiyayakko <reiyayakko@gmail.com>
2020-12-26 11:34:11 +0900

    use deep state

--- src/object/deep-explore/deep-explore.ts
+++ src/object/deep-explore/deep-explore.ts
@@ -1,104 +1,122 @@
 
-// import { has } from "../property/has";
+import { has } from "../property/has";
+import { DeepState } from "./state";
 
 interface PropertyProfile {
     // parent: any;
     descriptor: PropertyDescriptor;
-    depth: number;
-    deepest: boolean;
-    existings: Set<any> | Map<any, any>;
+    key: PropertyKey | null; // for root
+    value?: unknown;
+    // child?: unknown;
+    /** ルートからのプロパティーの配列です。 */
     path: PropertyKey[];
-    route: any[];
+    /** ルートからの参照オブジェクトです。 */
+    route: object[];
+    existings: Set<object> | Map<object, unknown>;
+
+    // value: unknown;
+    /** ルートを0とした参照の深さ */
+    depth: number;
+
+    isDive: boolean;
+    isDeepest: boolean;
+    isAccessor: boolean;
+    isExisting: boolean;
+    /** 再帰参照 */
+    isRecursiveReference: boolean;
+
+    // /**
+    //  * nullishを渡すとcontinue、その他でそれを探索
+    //  * @param value
+    //  */
+    // dive(value: unknown): void;
 }
+
 export interface Options {
     depthLimit?: number;
     useMap?: boolean;
-    report?: boolean;
+    // report?: boolean;
+    // useDescriptor?: boolean;
     // exclude?: PropertyKey[][];
     // methods
-    keys?(o: any): PropertyKey[];
-    shouldExplore?(o: any): boolean;
+    keys?(o: object): PropertyKey[];
+    shouldExplore?(o: unknown): boolean;
     // hooks
-    every?(s: InternalState): void,
-    recursive?(s: InternalState): void,
-    propertyWillDive?(s: PropertyProfile): void,
-    property?(s: InternalState): void,
-    propertyDidDive?(s: PropertyProfile): void,
+    // every?(s: InternalState): boolean;
+    // every?(s: InternalState, s2: InternalState): boolean;
+    property?(profile: PropertyProfile): void | Function;
 }
-export interface InternalState {
-    // methods
-    keys(o: any): PropertyKey[];
-    shouldExplore(o: any): boolean;
-    invoke(t: string, o?: any): void;
-    // constants
-    depthLimit: number;
-    options: Options;
-    // state
-    existings: Set<any> | Map<any, any>;
-    path: PropertyKey[];
-    route: any[];
-    report: Report;
-    depth(): number;
-}
+
 export interface Report {
     maxDepth: number;
     skip: number;
     break: number;
     exit: boolean;
-    hasRecursive: boolean;
+    hasCyclic: boolean;
 }
 
 // TODO: Map, Set等の対応。
 // IDEA: ユーザーが任意に拡張できるようする?
-export const deepExplore = (target: any, state: InternalState): void => {
+export const deepExplore = (target: unknown, state: DeepState): void => {
     const isExplore = state.shouldExplore(target);
 
-    state.invoke("every", { isExplore });
+    // state.invoke("every", { isExplore });
 
     if(!isExplore) return;
 
-    if("add" in state.existings)
-        state.existings.add(target);
-    // else state.existings.set(target, state.ref);
+    // if("add" in state.existings)
+    //     state.existings.add(target);
+    // // else state.existings.set(target, state.ref);
     state.route.push(target);
 
     const depth = state.depth();
-    const deepest = depth > state.depthLimit;
+    const isDeepest = depth > state.depthLimit;
 
     const keys = state.keys(target);
     for(let i = 0;i < keys.length;) {
         const key = keys[i++];
-        const descriptor = Object.getOwnPropertyDescriptor(target, key);
+        if(has(target, key)) continue;
+
+        const useDescriptor = !!state.options.useDescriptor;
+
+        const descriptor = useDescriptor ? Object.getOwnPropertyDescriptor(target, key) : null;
+        let child = useDescriptor ? target[key] : descriptor!.value;
 
         state.path.push(key);
 
-        const isValue = descriptor && "value" in descriptor;
-        const existingProperty = state.existings.has(descriptor.value);
-        if(existingProperty) {
-            const recursiveReference = state.route.includes(descriptor.value);
+        const isAccessor = useDescriptor && descriptor ? !("value" in descriptor) : false;
+        const isExisting = state.existings.has(child); // Again
+        const isRecursiveReference = isExisting && state.route.includes(child);
+        if(isRecursiveReference) state.report.hasCyclic = true;
+        let isDive = !(isDeepest || isRecursiveReference || isAccessor);
+        // const isExplore = isDive && state.shouldExplore(target);
 
-            state.invoke("propertyAgain", { ...state, path: [...state.path], descriptor });
-            type propertyAgain = (obj: unknown) => asserts obj is Object;
-            const propertyAgain = ({}) => ({});
-            if(typeof propertyAgain === "function") {
-                propertyAgain({ state, descriptor, depth, deepest });
-            }
-
-            if(recursiveReference) {
-                state.report.hasRecursive = true;
-                state.invoke("recursive", { ...state, path: [...state.path], descriptor });
-            }
-        } else {
-            state.invoke("property", { ...state, path: [...state.path], descriptor });
-            // invokeHook("property", props, propDesc);
-            // && state.isDive()
-            if(!deepest && isValue) {
-                // invokeHook("propertyWillDive", props, propDesc);
-                state.invoke("propertyWillDive");
-                deepExplore(descriptor.value, state);
-                // invokeHook("propertyDidDive", props, propDesc);
-                state.invoke("propertyDidDive");
-            }
+        const propertyProfile: PropertyProfile = {
+            path: state.path,
+            route: state.route,
+            existings: state.existings,
+            ...descriptor,
+            descriptor: descriptor!,
+            key,
+            // value: child,
+            depth,
+            isDive,
+            isDeepest,
+            // isExplore,
+            isAccessor,
+            isExisting,
+            isRecursiveReference,
+            // dive(value: unknown) {
+            //     isDive = value != null;
+            //     child = value;
+            // }
+        };
+        const didDive = state.options.property(propertyProfile);
+        if(isDive) {
+            // if(isExplore) {
+            deepExplore(child, state);
+            // } else ;
+            if(didDive) didDive();
         }
         state.path.pop();
     }
--- src/object/deep-explore/explore.ts
+++ src/object/deep-explore/explore.ts
@@ -1,9 +1,6 @@
 
-import { isFunction } from "../../util/is/function";
-import { MAX_BIT_NUMBER } from "../../number/util/constant";
-import { isObject } from "../is/object";
-import { getKeys } from "../standard/keys";
-import { deepExplore, InternalState, Options, Report } from "./deep-explore";
+import { deepExplore, Options, Report } from "./deep-explore";
+import { DeepState } from "./state";
 
 /**
  *
@@ -13,30 +10,7 @@ import { deepExplore, InternalState, Options, Report } from "./deep-explore";
  * const report = deepExplore(target);
  */
 export const explore = (target: unknown, options: Options={}): Report => {
-    const state: InternalState = {
-        keys: isFunction(options.keys)
-            ? options.keys : getKeys,
-        shouldExplore: isFunction(options.shouldExplore)
-            ? options.shouldExplore : isObject,
-        depthLimit: (options.depthLimit as number) < MAX_BIT_NUMBER
-            ? (options.depthLimit as number) | 0 : Infinity,
-        existings: options.useMap ? new Map : new Set,
-        path: [],
-        route: [],
-        report: {
-            maxDepth: 0,
-            skip: 0,
-            break: 0,
-            exit: false,
-            hasRecursive: false,
-        },
-        options,
-        depth() { return state.route.length; },
-        invoke(type, option) {
-            if(type in this.options)
-                this.options[type](option);
-        },
-    };
+    const state = new DeepState(options);
     deepExplore(target, state);
     return state.report;
 };
