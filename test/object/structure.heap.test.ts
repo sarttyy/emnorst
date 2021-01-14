f23bf3724fe649b7af0a7f60e65428c272c1751b
reiyayakko <reiyayakko@gmail.com>
2021-01-14 14:03:47 +0900

    test: add test for number comparator

--- test/object/structure.heap.test.ts
+++ test/object/structure.heap.test.ts
@@ -14,6 +14,7 @@ describe("Heap", () => {
         _case(v => ({ val: v }), v => v.val, (l, r) => l.val > r.val),
         _case(v => new Number(v), v => v.valueOf(), false),
         _case(v => new Number(v), v => v.valueOf(), true),
+        _case(v => v, v => v, (l, r) => r - l),
     ])("Correct take-out order (case-%#)", (mapper, getter, compareOrder, gt) => {
         const originalData = [5, 1, 4, 3, 0, 6, 8, 9, 2, 7];
         const initData = originalData.map(mapper as any);
@@ -36,8 +37,13 @@ describe("Heap", () => {
     test("sort", () => {
         const sortedArray1 = Heap.sort([2, 7, 3, 0, 5, 1, 8, 4, 6], (l, r) => l < r);
         expect(sortedArray1).toEqual([...Array(9).keys()]);
+
         const sortedArray2 = Heap.sort([5, 1, 4, 3, 0, 6, 8, 9, 2, 7]);
         expect(sortedArray2).toEqual([...Array(10).keys()]);
+
+        const fn = (l: number, r: number) => r - l;
+        const sortedArray3 = Heap.sort([5, 1, 4, 3, 0, 6, 8, 9, 2, 7], fn);
+        expect(sortedArray3).toEqual([...sortedArray3].sort(fn));
     });
     test("heapify", () => {
         const data = Heap.heapify([5, 1, 4, 3, 0, 6, 8, 9, 2, 7]);
