08c72e698fcc3e3df121e89b3c4ad53ffa891f0b
reiyayakko <reiyayakko@gmail.com>
2021-04-16 16:26:19 +0900

    feat: add type TupleableToArray

--- src/object/standard/types.ts
+++ src/object/standard/types.ts
@@ -3,3 +3,5 @@ interface EmptyObject { [key: string]: never }
 interface EmptyArrayLike { length: 0 }
 
 export type Empty = "" | [] | EmptyArrayLike | EmptyObject;
+
+export type TupleableToArray<T> = T extends (infer U)[] ? U[] : T;
