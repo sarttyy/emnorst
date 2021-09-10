6c34a0de46ac08e6e33017a81c6b35fb02c8b088
rizzzse <rizzzse@gmail.com>
2021-09-11 00:08:46 +0900

    feat: exclude extra `number` type

--- src/object/property/to-property-key.ts
+++ src/object/property/to-property-key.ts
@@ -1,6 +1,6 @@
 import { toPrimitive } from "~/util/primitive";
 
-export const toPropertyKey = (value: unknown): PropertyKey => {
+export const toPropertyKey = (value: unknown): string | symbol => {
     const key = toPrimitive(value, "string");
     return typeof key === "symbol" ? key : String(key);
 };
