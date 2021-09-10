a004fe12b637dae77481c545e5f6757b415c403e
rizzzse <rizzzse@gmail.com>
2021-09-11 00:13:18 +0900

    feat: add ~/object to index

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,11 +1,2 @@
-
-export * from "./arraylike";
-export * from "./clone";
-export * from "./deep-explore";
-export * from "./iter";
-export * from "./property";
-export * from "./standard";
 export * from "./is";
-export * from "./structure";
-
-// export * from "./zip.js";
+export * from "./property";
