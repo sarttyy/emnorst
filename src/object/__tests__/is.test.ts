a059aef773d32771cb836c2847605e12f5e1adad
rizzzse <rizzzse@gmail.com>
2021-09-11 00:57:44 +0900

    test: add test for object/is/**

--- src/object/__tests__/is.test.ts
+++ src/object/__tests__/is.test.ts
@@ -1,5 +1,5 @@
 
-import { isEmpty } from "../is";
+import { isEmpty, isArrayLike, isObject, isPureObject } from "../is";
 
 describe("is", () => {
     test("isEmpty", () => {
@@ -11,5 +11,24 @@ describe("is", () => {
         expect(isEmpty({ prop: "not empty" })).toBeFalsy();
         expect(isEmpty(true)).toBeFalsy();
         expect(isEmpty(0)).toBeFalsy();
+    });
+    test("isArrayLike", () => {
+        expect(isArrayLike([])).toBeTruthy();
+        expect(isArrayLike("isArrayLike")).toBeTruthy();
+        expect(isArrayLike({ length: 0 })).toBeTruthy();
+        expect(isArrayLike({ length: -1 })).toBeFalsy();
+        expect(isArrayLike({})).toBeFalsy();
+        expect(isArrayLike(() => { /* noop */ })).toBeFalsy();
+        expect(isArrayLike(null)).toBeFalsy();
+    });
+    test("isObject", () => {
+        expect(isObject({})).toBeTruthy();
+        expect(isObject([])).toBeTruthy();
+        expect(isObject(Object(""))).toBeTruthy();
+        expect(isObject(null)).toBeFalsy();
+    });
+    test("isPureObject", () => {
+        expect(isPureObject({})).toBeTruthy();
+        expect(isPureObject(new class {})).toBeFalsy();
     });
 });
