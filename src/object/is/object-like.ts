cf63857bc908ff34e0852d111c48c95c8bf16f0c
rizzzse <rizzzse@gmail.com>
2021-09-10 23:50:19 +0900

    feat: remove unneeded "is" functions

--- src/object/is/object-like.ts
+++
@@ -1,7 +0,0 @@
-
-import { isFunction } from "util/is/function";
-import { isObject } from "./object";
-
-export const isObjectLike = (value: unknown): value is object => (
-    isObject(value) || isFunction(value)
-);
