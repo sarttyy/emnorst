92cc39425599a9127fdcdd2d95a72c6dc833f4bf
rizzzse <rizzzse@gmail.com>
2021-09-13 02:32:26 +0900

    fix: add type check

--- src/util/type-of.ts
+++ src/util/type-of.ts
@@ -76,8 +76,10 @@ export const typeOf = <T>(value: T): TypeOf<T> => {
     }
 
     if(toStringTag(value) === "Object") {
-        const ctor = (value as unknown as object).constructor;
-        if(ctor?.name) return ctor.name as TypeOf<T>;
+        const ctorName: unknown = (value as unknown as object).constructor?.name;
+        if(typeof ctorName === "string" && ctorName) {
+            return ctorName as TypeOf<T>;
+        }
     }
 
     return prevResult as TypeOf<T>;
