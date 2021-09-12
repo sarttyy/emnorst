9e19b9b94809652908ad49e395e6d8b13e90e4e2
rizzzse <rizzzse@gmail.com>
2021-09-13 02:49:31 +0900

    chore: remove unused type file

--- src/object/standard/types.ts
+++
@@ -1,7 +0,0 @@
-
-interface EmptyObject { [key: string]: never }
-interface EmptyArrayLike { length: 0 }
-
-export type Empty = "" | [] | EmptyArrayLike | EmptyObject;
-
-export type TupleableToArray<T> = T extends (infer U)[] ? U[] : T;
