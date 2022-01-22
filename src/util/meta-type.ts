0edd6b87a8ace25bf479188bdd3305cfd3e9fa07
rizzzse <rizzzse@gmail.com>
2022-01-23 00:35:16 +0900

    fix: make it simple type

--- src/util/meta-type.ts
+++ src/util/meta-type.ts
@@ -1,35 +1,4 @@
-// Meta
+export type HasMeta<B, M> = { "__?META": [B, M] };
 
-declare class HasMeta<B, M> {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    private __EMNORST_META_BASE: B;
-    private __EMNORST_META_DATA: M;
-    /* eslint-enable */
-}
-type WithMeta<Base, M> = Base & HasMeta<Base, M>;
-
-// dprint-ignore
-export type Meta<Base, M> =
-    Base extends WithMeta<infer PureBase, infer B>
-        ? WithMeta<PureBase, Meta<B, M>>
-    : Base extends HasWeekMeta<infer PureBase, infer B>
-        ? WithMeta<PureBase, WeekMeta<B, M>>
-    : WithMeta<Base, M>;
-
-// WeekMeta
-
-declare class HasWeekMeta<B, M> {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    private __EMNORST_WEEK_META_BASE?: B;
-    private __EMNORST_WEEK_META_DATA?: M;
-    /* eslint-enable */
-}
-type WithWeekMeta<Base, M> = Base & HasWeekMeta<Base, M>;
-
-// dprint-ignore
-export type WeekMeta<Base, M> =
-    Base extends WithMeta<infer PureBase, infer B>
-        ? WithWeekMeta<PureBase, Meta<B, M>>
-    : Base extends HasWeekMeta<infer PureBase, infer B>
-        ? WithWeekMeta<PureBase, WeekMeta<B, M>>
-    : WithWeekMeta<Base, M>;
+export type Meta<Base, M> = Base & HasMeta<Base, M>;
+export type WeekMeta<Base, M> = Base & Partial<HasMeta<Base, M>>;
