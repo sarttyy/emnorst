d55ab68450edfc82ead56fbc706aa4242409bfb0
rizzzse <rizzzse@gmail.com>
2022-01-24 20:35:32 +0900

    chore: remove unused files

--- src/util/compare/types.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * @template T item
- * @template U direction
- */
-export type Comparator<T=unknown, U=number> = (left: T, right: T) => U;
-
-export type CompareOrder<T> = boolean | Comparator<T, number | boolean | null>;
