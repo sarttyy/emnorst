3126e27b836e0ae200fa038ac3f95f87fe8550e5
rizzzse <rizzzse@gmail.com>
2022-01-24 23:08:19 +0900

    feat: add NonUnion

--- src/util/type.ts
+++ src/util/type.ts
@@ -9,3 +9,19 @@ export type Recurse<T, K extends string = "__rec"> =
 
 export type Repeat<T, U extends number, V extends unknown[] = []> =
     V["length"] extends U ? V : Repeat<T, U, [...V, T]>;
+
+/**
+ * @example
+ * interface Emittable<T> {
+ *     emit<U extends keyof T>(type: NonUnion<U>, x: T[NonUnion<U>]): void;
+ * }
+ * declare const emittable: Emittable<{ foo: string, bar: number }>;
+ *
+ * emittable.emit<"foo">("foo", "hello"); // ok
+ * emittable.emit<"foo" | "bar">("foo", 1234); // error!!!
+ */
+export type NonUnion<T> = { x: T } extends { x: infer U }
+    ? T extends unknown
+        ? [U] extends [T] ? T : never
+        : never
+    : never;
