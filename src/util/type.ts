d886ca004d1a4336bbe889c0b0e7eeca6b5076c4
rizzzse <rizzzse@gmail.com>
2022-01-24 23:01:32 +0900

    feat: support other properties

--- src/util/type.ts
+++ src/util/type.ts
@@ -1,10 +1,11 @@
-type RecurseSub<T> =
-    T extends { __rec: never } ? never
-    : T extends { __rec: { __rec: infer U } } ? { __rec: RecurseSub<U> }
-    : T extends { __rec: infer U } ? U
+type RecurseSub<T, K extends string> =
+    T extends { [P in K]: never } ? never
+    : T extends { [P in K]: { [P in K]: infer U } } ? { [P in K]: RecurseSub<U, K> }
+    : T extends { [P in K]: infer U } ? U
     : T;
 
-export type Recurse<T> = T extends { __rec: unknown } ? Recurse<RecurseSub<T>> : T;
+export type Recurse<T, K extends string = "__rec"> =
+    T extends { [P in K]: unknown } ? Recurse<RecurseSub<T, K>> : T;
 
 export type Repeat<T, U extends number, V extends unknown[] = []> =
     V["length"] extends U ? V : Repeat<T, U, [...V, T]>;
