2a6719831fd616c3b1af5f67533bb0948e0dee7b
rizzzse <rizzzse@gmail.com>
2022-03-04 22:03:58 +0900

    chore: deprecate `isPureObject`

--- src/object/is/pure-object.ts
+++ src/object/is/pure-object.ts
@@ -3,6 +3,7 @@ import { typeOf } from "~/util/type-of";
 
 type PureObject = WeakMeta<object, { pure: true }>;
 
+/** @deprecated */
 export const isPureObject = (value: unknown): value is PureObject =>
     typeOf(value) === "Object"
     && (value as object).constructor === Object;
