a81d144cfbcbef0cc07f72ef7d84640eff70cb50
rizzzse <rizzzse@gmail.com>
2022-03-24 00:26:37 +0900

    test: add test for `isEnumerableProp` and `isKey`

--- src/object/__tests__/property.test.ts
+++ src/object/__tests__/property.test.ts
@@ -1,10 +1,41 @@
 
-import { getAllKeys, getEnumerableKeys, has, toPropertyKey } from "../property";
+import { getAllKeys, getEnumerableKeys, has, isEnumerableProp, isKey, toPropertyKey } from "../property";
 
 describe("property", () => {
+    test("toPropertyKey", () => {
+        expect(toPropertyKey("string")).toBe("string");
+        expect(toPropertyKey(42)).toBe("42");
+        expect(toPropertyKey(Symbol.for("symbol"))).toBe(Symbol.for("symbol"));
+        expect(toPropertyKey({ toString: () => 1, valueOf: () => 2 })).toBe("1");
+    });
+    test("isKey", () => {
+        expect(isKey("")).toBeTruthy();
+        expect(isKey(0)).toBeTruthy();
+        expect(isKey(Symbol())).toBeTruthy();
+        expect(isKey({})).toBeFalsy();
+    });
+    const obj = Object.defineProperty(
+        Object.create({ protoProp: null }),
+        "nonEnumerableProp",
+        {},
+    );
+    obj.prop = null;
     test("has", () => {
-        expect(has({ hoge: null }, "hoge")).toBeTruthy();
-        expect(has(Object.create({ hoge: null }), "hoge")).toBeFalsy();
+        expect(has(obj, "prop")).toBeTruthy();
+        expect(has(obj, "nonEnumerableProp")).toBeTruthy();
+        expect(has(obj, "protoProp")).toBeFalsy();
+    });
+    test("isEnumerableProp", () => {
+        expect(isEnumerableProp(obj, "prop")).toBeTruthy();
+        expect(isEnumerableProp(obj, "nonEnumerableProp")).toBeFalsy();
+        expect(isEnumerableProp(obj, "protoProp")).toBeFalsy();
+    });
+    test("getEnumerableKeys", () => {
+        const keys = ["hoge", "huga", Symbol("piyo")];
+        const obj = Object.fromEntries(keys.map(key => [key, null]));
+        Object.defineProperty(obj, "nonEnumerableProp", {});
+        Object.defineProperty(obj, Symbol("nonEnumerableSymbolProp"), {});
+        expect(getEnumerableKeys(obj)).toEqual(keys);
     });
     test("getAllKeys", () => {
         expect(getAllKeys({})).toEqual([]);
@@ -12,18 +43,5 @@ describe("property", () => {
         Object.defineProperty(obj, "huga", {});
         Object.defineProperty(obj, Symbol.for("piyo"), {});
         expect(getAllKeys(obj)).toEqual(["hoge", "huga", Symbol.for("piyo")]);
-    });
-    test("getEnumerableKeys", () => {
-        const keys = ["hoge", "huga", Symbol("piyo")];
-        const obj = Object.fromEntries(keys.map(key => [key, null]));
-        Object.defineProperty(obj, "non enumerable prop name", {});
-        Object.defineProperty(obj, Symbol("non enumerable symbol"), {});
-        expect(getEnumerableKeys(obj)).toEqual(keys);
-    });
-    test("toPropertyKey", () => {
-        expect(toPropertyKey("string")).toBe("string");
-        expect(toPropertyKey(42)).toBe("42");
-        expect(toPropertyKey(Symbol.for("symbol"))).toBe(Symbol.for("symbol"));
-        expect(toPropertyKey({ toString: () => 1, valueOf: () => 2 })).toBe("1");
     });
 });
