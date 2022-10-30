0a95b0c4b342ace8df433fb2cfeb150d6ad8688d
rizzzse <rizzzse@gmail.com>
2022-10-30 22:21:43 +0900

    refactor: 変数のrenameとか

--- src/object/property.ts
+++ src/object/property.ts
@@ -21,20 +21,21 @@ const getPropSymbols = Object.getOwnPropertySymbols;
 export type KeyOf<T> = (T extends unknown ? keyof T : never) | (string & {}) | (number & {}) | symbol;
 export type ValueOf<T> = T[keyof T];
 
-export const has = <T, U extends KeyOf<T>>(obj: T, key: U):
-    obj is Normalize<U extends unknown ? T & { [_ in U]: unknown } : never> =>
-    obj != null && prototypeHasOwnProperty.call(obj, key);
+export const has = <T, U extends KeyOf<T>>(object: T, key: U):
+    object is Normalize<U extends unknown ? T & { [_ in U]: unknown } : never> => {
+    return object != null && prototypeHasOwnProperty.call(object, key);
+};
 
-export const isEnumerableProp = <T, U extends KeyOf<T>>(obj: T, key: U):
-    obj is Normalize<U extends unknown ? T & { [_ in U]: unknown } : never> =>
-    obj != null && prototypePropertyIsEnumerable.call(obj, key);
+export const isEnumerableProp = <T, U extends KeyOf<T>>(object: T, key: U):
+    object is Normalize<U extends unknown ? T & { [_ in U]: unknown } : never> => {
+    return object != null && prototypePropertyIsEnumerable.call(object, key);
+};
 
-type Swapable<T, K extends keyof T> =
-    {
-        [_ in K]: Intersection<
-            K extends unknown ? { x: T[K] } : never
-        > extends { x: infer U } ? U : never;
-    };
+type Swapable<T, K extends keyof T> = {
+    [_ in K]: Intersection<
+        K extends unknown ? { x: T[K] } : never
+    > extends { x: infer U } ? U : never;
+};
 
 /**
  * @example
@@ -57,16 +58,15 @@ export const keys: <T extends object>(object: T) => KeyOf<T>[] = Object.keys;
 /**
  * Returns enumerable properties of an object.
  *
- * @param obj Object to get properties.
  * @returns An array of enumerable properties containing symbols.
  */
-export const getEnumerableKeys = (obj: object): (string | symbol)[] => {
-    const keys: (string | symbol)[] = Object.keys(obj);
-    const symbols = getPropSymbols(obj);
+export const getEnumerableKeys = (object: object): (string | symbol)[] => {
+    const keys: (string | symbol)[] = Object.keys(object);
+    const symbols = getPropSymbols(object);
     for(let i = 0; i < symbols.length; i++) {
         const symbol = symbols[i];
 
-        if(prototypePropertyIsEnumerable.call(obj, symbol)) {
+        if(prototypePropertyIsEnumerable.call(object, symbol)) {
             keys.push(symbol);
         }
     }
@@ -76,8 +76,9 @@ export const getEnumerableKeys = (obj: object): (string | symbol)[] => {
 /**
  * Returns all properties of the object.
  *
- * @param obj Object to get properties.
  * @returns An array of all properties, including symbols and non-enumerable.
  */
-export const getAllKeys = (obj: object): (string | symbol)[] =>
-    (getPropNames(obj) as (string | symbol)[]).concat(getPropSymbols(obj));
+export const getAllKeys = (object: object): (string | symbol)[] => {
+    const propNames: (string | symbol)[] = getPropNames(object);
+    return propNames.concat(getPropSymbols(object));
+};
