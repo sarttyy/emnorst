28eb34a15c9390e1601747ad72de4ec14646f0b2
rizzzse <rizzzse@gmail.com>
2022-10-30 23:49:50 +0900

    feat: add `SplitString` and `JoinString` types

--- src/string/types.ts
+++ src/string/types.ts
@@ -7,3 +7,34 @@
  * ```
  */
 export type TemplateLiteralConstraint = string | number | bigint | boolean | null | undefined;
+
+/**
+ * @example
+ * ```ts
+ * type Type = SplitString<"foo,bar,baz", ",">;
+ * // ["foo", "bar", "baz"]
+ * ```
+ */
+export type SplitString<T extends string, Separator extends TemplateLiteralConstraint> = (
+    T extends `${infer U}${Separator}${infer Rest}`
+        ? [U, ...SplitString<Rest, Separator>]
+        : [T]
+);
+
+/**
+ * @example
+ * ```ts
+ * type Type = JoinString<["foo", "bar", "baz"], ",">;
+ * // "foo,bar,baz"
+ * ```
+ */
+export type JoinString<T extends readonly TemplateLiteralConstraint[], Separator extends TemplateLiteralConstraint> = (
+    T extends readonly [
+        infer U extends TemplateLiteralConstraint,
+        ...infer Rest extends readonly TemplateLiteralConstraint[],
+    ]
+        ? Rest extends readonly [unknown, ...unknown[]]
+            ? `${U}${Separator}${JoinString<Rest, Separator>}`
+            : U
+    : ""
+);
